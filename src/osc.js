0d103a02a1c41801f994e5d1a77afc2fae279c6a
EmilioOcelotl <emilio.ocelotl@gmail.com>
2024-10-03 16:04:47 -0600

    g2 g3

--- src/osc.js
+++ src/osc.js
@@ -9,12 +9,13 @@ const audioloader = new FreeSoundAudioLoader(apiKey)
 
 const socket = new WebSocket(`ws://127.0.0.1:8080`);
 
-let rec, g1 = new Grain(audioCtx); 
+let rec, g1 = new Grain(audioCtx), g2 = new Grain(audioCtx), g3 = new Grain(audioCtx); 
+
+let g2buffer = 0, g3buffer = 0;
 
 socket.onmessage = (event) => {
   const oscMsg = JSON.parse(event.data);
   console.log('Mensaje OSC recibido en la web:', oscMsg);
-  console.log(parseFloat(oscMsg.numarg[0]))
 
   if(oscMsg.address == "/rec" && oscMsg.numarg[0] == 1){
     navigator.mediaDevices.getUserMedia({ audio: true })
@@ -55,11 +56,102 @@ socket.onmessage = (event) => {
   if(oscMsg.address == "/g1stop"){
     g1.stop();
     console.log("termina g1")
- 
   }
 
   if(oscMsg.address == "/g1gain"){
     g1.gain = oscMsg.numarg[0]; 
+    console.log("cambio ganancia de g1")
+  }
+
+  if(oscMsg.address == "/g2search"){
+    async function realizarBusqueda() {
+        const resultados = await freesound.buscar(oscMsg.numarg[0]);
+        return resultados; 
+        // luego una función que con los resultados busque un archivo en concreto y lo descargue
+    }
+
+    realizarBusqueda()
+    .then(resultados => {
+        let res = resultados.resultados[Math.floor(Math.random() * resultados.resultados.length)];
+        console.log(res)
+        let srchURL = 'https://freesound.org/apiv2/sounds/' + res.id; 
+        console.log("liga:" + srchURL);  
+        audioloader.loadAudio(srchURL)
+        .then(buffer => {
+            g2buffer = buffer; 
+            g2.set(buffer, 0, 0.5, 0.1, 0.1, 0.1)
+            //grain.load(buffer); 
+            g2.start(); 
+            console.log("muestra cargada")
+        })   
+    })
+    console.log("buscando g2")
+  }
+
+  if(oscMsg.address == "/g2set"){
+    g2.set(g2buffer, oscMsg.numarg[0], oscMsg.numarg[1], oscMsg.numarg[2], oscMsg.numarg[3], oscMsg.numarg[4]);  
+    console.log("g2 set")
+  }
+
+  if(oscMsg.address == "/g2start"){ 
+    g2.start(); 
+    console.log("inicia g2")
+  }
+
+  if(oscMsg.address == "/g2stop"){
+    g2.stop();
+    console.log("termina g2")
+  }
+
+  if(oscMsg.address == "/g2gain"){
+    g2.gain = oscMsg.numarg[0]; 
+    console.log("cambio ganancia de g2")
+  }
+
+  if(oscMsg.address == "/g3search"){
+    console.log("hola")
+    async function realizarBusqueda() {
+        const resultados = await freesound.buscar(oscMsg.numarg[0]);
+        return resultados; 
+        // luego una función que con los resultados busque un archivo en concreto y lo descargue
+    }
+
+    realizarBusqueda()
+    .then(resultados => {
+        let res = resultados.resultados[Math.floor(Math.random() * resultados.resultados.length)];
+        console.log(res)
+        let srchURL = 'https://freesound.org/apiv2/sounds/' + res.id; 
+        console.log("liga:" + srchURL);  
+        audioloader.loadAudio(srchURL)
+        .then(buffer => {
+            g3buffer = buffer; 
+            g3.set(buffer, 0, 0.5, 0.1, 0.1, 0.1)
+            //grain.load(buffer); 
+            g3.start(); 
+            console.log("muestra cargada")
+        })   
+    })
+    console.log("buscando g3")
+  }
+
+  if(oscMsg.address == "/g3set"){
+    g3.set(g3buffer, oscMsg.numarg[0], oscMsg.numarg[1], oscMsg.numarg[2], oscMsg.numarg[3], oscMsg.numarg[4]);  
+    console.log("g3 set")
+  }
+
+  if(oscMsg.address == "/g3start"){ 
+    g3.start(); 
+    console.log("inicia g3")
+  }
+
+  if(oscMsg.address == "/g3stop"){
+    g3.stop();
+    console.log("termina g3")
+  }
+
+  if(oscMsg.address == "/g3gain"){
+    g3.gain = oscMsg.numarg[0]; 
+    console.log("cambio ganancia de g3")
   }
 
 };
